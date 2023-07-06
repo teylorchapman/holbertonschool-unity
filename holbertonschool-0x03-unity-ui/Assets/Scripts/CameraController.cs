9c4966119d58b96effc73bb3ff3bd14b7fde7770
Teylor Chapman <5535@holbertonstudents.com>
2023-07-06 11:13:50 -0500

    i hope that this is correct bc its acting like it aint :L

---
+++ holbertonschool-0x03-unity-ui/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
+	public Vector3 cameraOffset;
+
+	void Start()
+	{
+		cameraOffset = this.transform.position - player.transform.position;
+		Debug.Log(cameraOffset);
+	}
+
+	void LateUpdate ()
+	{
+		if (player != null)
+		{
+			this.transform.position = player.transform.position + cameraOffset;
+		}
+	}
+}
---
+++ holbertonschool-0x03-unity-ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+	public Material trapMat;
+	public Material goalMat;
+	public Toggle colorblindMode;
+
+	public int MazeScene;
+	public void PlayMaze()
+	{
+		ToggleColorblindMode();
+		SceneManager.LoadScene(MazeScene);
+	}
+
+	public void QuitMaze()
+	{
+		Debug.Log("Quit Game");
+		#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+	}
+
+	public void ToggleColorblindMode()
+	{
+		if (colorblindMode.isOn)
+		{
+			trapMat.color = new Color32(255, 112, 0, 1);
+			goalMat.color = Color.blue;
+		}
+		else
+		{
+			trapMat.color = Color.red;
+			goalMat.color = Color.green;
+		}
+	}
+}
